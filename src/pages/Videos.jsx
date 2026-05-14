13f03fd188d2ce3cf7717570922ec9d92c301ccd
Darien Arthur-Gocken <233949874+DarienArthur-Gocken@users.noreply.github.com>
2026-05-14 10:33:50 -0700

    video loading state

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,17 +1,30 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getVideos } from '../mockApi.js';
 
 export default function Videos() {
+    const [videos, setVideos] = useState([]);
+    const [loading, setLoading] = useState(true);
+
     useEffect(() => {
         getVideos().then((data) => {
-            console.log("getVideos returned:", data);
+            setVideos(data)
+            setLoading(false)
+        
         });
     }, []);
 
+        if (loading) {
+        return <p>Loading videos...</p>;
+        }
+
     return (
         <div>
-            <h1>Available Videos</h1>
-            <p>Video list will go here.</p>
+        <h1>Available Videos</h1>
+        <ul>
+            {videos.map((filename) => (
+            <li key={filename}>{filename}</li>
+            ))}
+        </ul>
         </div>
     );
-}
+    }
