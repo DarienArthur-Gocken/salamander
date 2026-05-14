aeee8f806c91da1e3f24181ae8a14178f5b0d8dc
Darien Arthur-Gocken <233949874+DarienArthur-Gocken@users.noreply.github.com>
2026-05-14 10:26:15 -0700

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import Videos from './pages/Videos.jsx';
 import Preview from './pages/Preview.jsx';
@@ -6,11 +6,24 @@ import Export from './pages/Export.jsx';
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/videos" element={<Videos />} />
-      <Route path="/preview" element={<Preview />} />
-      <Route path="/export" element={<Export />} />
-    </Routes>
+    <div>
+
+      <nav>
+        <Link to="/">Home</Link>
+        {' | '}
+        <Link to="/videos">Videos</Link>
+        {' | '}
+        <Link to="/preview">Preview</Link>
+        {' | '}
+        <Link to="/export">Export</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/videos" element={<Videos />} />
+        <Route path="/preview" element={<Preview />} />
+        <Route path="/export" element={<Export />} />
+      </Routes>
+    </div>
   );
 }
