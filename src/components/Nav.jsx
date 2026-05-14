d4b9558b7f92fbad3247b7dfccdeafc2535dbf60
Lilian <210286008+liliann19@users.noreply.github.com>
2026-05-14 11:07:01 -0700

    added styling for nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,14 +2,24 @@ import { Link } from 'react-router-dom'
 
 export default function Nav() {
     return (
-        <nav>
-            <Link to="/">Home</Link>
-            {' | '}
-            <Link to="/videos">Videos</Link>
-            {' | '}
-            <Link to="/preview">Preview</Link>
-            {' | '}
-            <Link to="/export">Export</Link>
+        <nav className="flex justify-center mt-6">
+            <div className="flex gap-8 px-6 py-3">
+                <Link to="/" className="text-gray-700 font-medium hover:text-blue-600 transition duration-300">
+                    Home
+                </Link>
+
+                <Link to="/videos" className="text-gray-700 font-medium hover:text-blue-600 transition duration-300">
+                    Videos
+                </Link>
+
+                <Link to="/preview/test.mp4" className="text-gray-700 font-medium hover:text-blue-600 transition duration-300">
+                    Preview
+                </Link>
+
+                <Link to="/export" className="text-gray-700 font-medium hover:text-blue-600 transition duration-300">
+                    Export
+                </Link>
+            </div>
         </nav>
     )
 }
