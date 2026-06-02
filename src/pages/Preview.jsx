7b7e80b9dac1030f9d080eb31bf416e283518e13
Darien Arthur-Gocken <233949874+DarienArthur-Gocken@users.noreply.github.com>
2026-06-02 10:24:40 -0700

    mockapi to api calls

--- src/pages/Preview.jsx
+++ src/pages/Preview.jsx
@@ -1,6 +1,6 @@
 import { Link, useParams } from 'react-router-dom';
 import { useRef, useEffect, useState } from 'react';
-import { getThumbnail } from '../mockApi.js';
+import { getThumbnail } from '../api.js';
 
 export default function Preview() {
     const { filename } = useParams();
